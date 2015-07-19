f60a529367879defaa3cbc5261203db3d0525396
JKereliuk <j.kereliuk@mail.utoronto.ca>
2015-07-19 05:32:25 -0400

    Connects when phones don't know about each other before

--- app/src/main/java/com/charity/battle/fightforcharity/MainActivity.java
+++ app/src/main/java/com/charity/battle/fightforcharity/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +26,7 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.UUID;
@@ -91,6 +94,15 @@ public class MainActivity extends ActionBarActivity {
 
     private InputStream in;
 
+    private final Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg)
+        {
+            System.out.println(msg.getData().toString());
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,6 +130,7 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v)
             {
                 AcceptThread acceptThread = new AcceptThread();
+                acceptThread.interrupt();
                 acceptThread.start();
             }
         });
@@ -171,6 +184,7 @@ public class MainActivity extends ActionBarActivity {
                 String macAddress = deviceSpinner.getSelectedItem().toString().split("\n")[1];
                 BluetoothDevice host = bluetoothAdapter.getRemoteDevice(macAddress);
                 ConnectThread connectThread = new ConnectThread(host);
+                connectThread.interrupt();
                 connectThread.start();
             }
         });
@@ -263,15 +277,15 @@ public class MainActivity extends ActionBarActivity {
             devicesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
             Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
             // If there are paired devices
-            if (pairedDevices.size() > 0) {
-                // Loop through paired devices
-                for (BluetoothDevice device : pairedDevices) {
-                    // Add the name and address to an array adapter to show in a ListView
-                    if (device.getName().endsWith(NAME_SUFFIX)) {
-                        devicesAdapter.add(device.getName() + "\n" + device.getAddress());
-                    }
-                }
-            }
+//            if (pairedDevices.size() > 0) {
+//                // Loop through paired devices
+//                for (BluetoothDevice device : pairedDevices) {
+//                    // Add the name and address to an array adapter to show in a ListView
+//                    if (device.getName().endsWith(NAME_SUFFIX)) {
+//                        devicesAdapter.add(device.getName() + "\n" + device.getAddress());
+//                    }
+//                }
+//            }
         }
 
         // Register the BroadcastReceiver
@@ -386,6 +400,9 @@ public class MainActivity extends ActionBarActivity {
 
             // Do work to manage the connection (in a separate thread)
 //            manageConnectedSocket(mmSocket);
+            ConnectedThread connectedThread = new ConnectedThread(mmSocket);
+            connectedThread.start();
+            connectedThread.write("hi".getBytes());
         }
 
         /** Will cancel an in-progress connection, and close the socket */
@@ -393,6 +410,62 @@ public class MainActivity extends ActionBarActivity {
             try {
                 mmSocket.close();
             } catch (IOException e) { }
+        }
+    }
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    handler.obtainMessage(1, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
         }
     }
 }
