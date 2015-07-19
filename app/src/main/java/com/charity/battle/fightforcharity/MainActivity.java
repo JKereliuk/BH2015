a00326b7e82d7bab467c2ec203d5614a31844908
gekath <kathyge@gmail.com>
2015-07-19 01:13:07 -0400

    Set up Paypal

--- app/src/main/java/com/charity/battle/fightforcharity/MainActivity.java
+++ app/src/main/java/com/charity/battle/fightforcharity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.charity.battle.fightforcharity;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +19,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.paypal.android.sdk.payments.PayPalAuthorization;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalService;
+
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
 
@@ -22,12 +31,30 @@ import static android.widget.Toast.makeText;
 public class MainActivity extends ActionBarActivity {
 
     private Button searchButton;
+    private Button paypalButton;
     private TextView mainText;
     private ImageView brainTreeLogo;
     PrefsFragment mPrefsFragment = new PrefsFragment();
     public boolean settings_toggle = true;
 
+    private static PayPalConfiguration config = new PayPalConfiguration()
+
+            // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
+            // or live (ENVIRONMENT_PRODUCTION)
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+
+            .clientId("ARYcSz8eGomVVQ-TPSuqQmM61u_PsHiD8WmQ05jNLfUccTu6fmPHwwhtNF88m0Tt5IQYn47rJrCrCAN2")
+
+            .merchantName("Donation Duel")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
     UserManager usermanager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +65,24 @@ public class MainActivity extends ActionBarActivity {
         searchButton = (Button) findViewById(R.id.searchButton);
         mainText = (TextView) findViewById(R.id.mainText);
         brainTreeLogo = (ImageView) findViewById(R.id.brainTree);
+        paypalButton = (Button) findViewById(R.id.config_paypal);
 
         usermanager = new UserManager(getApplicationContext());
 
         usermanager.checkLogin();
+
+        paypalButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View pressed) {
+                Intent intent = new Intent(MainActivity.this, PayPalFuturePaymentActivity.class);
+
+                // send the same configuration for restart resiliency
+                intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+                startActivityForResult(intent, REQUEST_CODE_FUTURE_PAYMENT);
+            }
+        });
+
 
         searchButton.setOnClickListener(new View.OnClickListener() {
 
@@ -60,6 +101,11 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        Intent intent = new Intent(this, PayPalService.class);
+
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        startService(intent);
     }
 
 
@@ -111,5 +157,33 @@ public class MainActivity extends ActionBarActivity {
         return preferences.getString(key, null);
     }
 
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            PayPalAuthorization auth = data
+                    .getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+            Log.d("TAG PLEASE TAG PLLZZZ", "POOOOOOOOP");
+//            if (auth != null) {
+//                try {
+//                    String authorization_code = auth.getAuthorizationCode();
+//
+//                    sendAuthorizationToServer(auth);
+//
+//                } catch (JSONException e) {
+//                    Log.e("FuturePaymentExample", "an extremely unlikely failure occurred: ", e);
+//                }
+//            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("FuturePaymentExample", "The user canceled.");
+        } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("FuturePaymentExample",
+                    "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+        }
+    }
 }
